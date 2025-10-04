28e5953f415fb0d3f56e62bd89fe022e51df1cc8
Elie Kanaan <eliekanaan91@gmail.com>
2025-10-04 19:25:03 +0300

    Assignment 2

---
+++ prime-number.py
@@ -0,0 +1,33 @@
+username = 'admin'
+password = '1234'
+tries_left = 3
+import math
+
+while tries_left > 0:
+    a = input("Enter your username: ")
+    b = input("Enter your password: ")
+
+    if a==username and b==password:
+        print ('you are logged in')
+        break
+    else:
+        tries_left-=1
+        print("Your username or password are wrong, try again")
+
+if tries_left==0:
+    print("Your account is blocked")
+else:
+    n=int(input("Please enter a number: "))
+    print(f"Prime numbers between 2 and {n} are:")
+    
+    
+    for num in range(2, n + 1):
+        is_prime = True
+    
+        for i in range(2, num):
+            if num % i == 0:
+             is_prime = False
+             break
+    
+        if is_prime:
+            print(num, end=" ")
